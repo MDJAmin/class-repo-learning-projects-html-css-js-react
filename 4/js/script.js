7e58c9dfe394b9792fbe0357bdf231ac5f60cc80
MDJAmin <mohammadamindf@gmail.com>
2024-10-21 20:59:17 +0330

    JS

---
+++ 4/js/script.js
@@ -0,0 +1,11 @@
+// const homePageNumberOne = "hi"
+// const HomePageNumberOne = "Hi"
+// const Home_Page_Number_One= "hi"
+"use strict"
+let Answer = prompt("What is the “official” name of JavaScript?")
+
+if(Answer == "ECMAScript"){
+alert("Right!")
+}else{
+    alert("You don’t know? ECMAScript!")
+}
