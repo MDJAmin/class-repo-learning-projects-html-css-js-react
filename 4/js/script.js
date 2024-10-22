f11eafb8c365a4e397bfb330f6b17e042a89c15f
MDJAmin <mohammadamindf@gmail.com>
2024-10-22 09:52:24 +0330

    js

--- 4/js/script.js
+++ 4/js/script.js
@@ -1,11 +1,13 @@
+"use strict"
+
 // const homePageNumberOne = "hi"
 // const HomePageNumberOne = "Hi"
 // const Home_Page_Number_One= "hi"
-"use strict"
-let Answer = prompt("What is the “official” name of JavaScript?")
 
-if(Answer == "ECMAScript"){
-alert("Right!")
-}else{
-    alert("You don’t know? ECMAScript!")
-}
+// let Answer = prompt("What is the “official” name of JavaScript?")
+
+// if(Answer == "ECMAScript"){
+// alert("Right!")
+// }else{
+//     alert("You don’t know? ECMAScript!")
+// }
