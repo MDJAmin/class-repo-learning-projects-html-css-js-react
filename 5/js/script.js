542cb7fbdc00197f475121ebbde8da4ac068a4d8
MDJAmin <mohammadamindf@gmail.com>
2024-11-03 15:30:27 +0330

    Fix Code's

--- 5/js/script.js
+++ 5/js/script.js
@@ -39,14 +39,15 @@ function plus() {
     return false;
   }
   if (spinnerValue >= 4) {
-    meter.style.background = "rgb(200,200,10)";
+    meter.style.background = "rgb(211, 211, 9)";
     meter.style.filter =
-      " drop-shadow(0 0 2px rgb(200,200,10)) drop-shadow(0 10px rgb(200,200,10));";
+      "drop-shadow(0 0 2.5px rgb(229, 255, 0)) drop-shadow(0 0 10px rgb(229, 255, 0))";
   }
   if (spinnerValue >= 7) {
     meter.style.background = "#f00";
-    meter.style.filter = "drop-shadow(0 0 2px #f00) drop-shadow(0 0 10px #f00)";
+    meter.style.filter =
+      "drop-shadow(0 0 2.5px #f00) drop-shadow(0 0 10px #f00)";
   }
-output.innerHTML = ++spinnerValue
-meter.style.height = spinnerValue * 10 + "%"
+  output.innerHTML = ++spinnerValue;
+  meter.style.height = spinnerValue * 10 + "%";
 }
