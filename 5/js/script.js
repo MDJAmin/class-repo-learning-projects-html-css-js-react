60c1d3377b38dcec065d6958af45400b2dfbf12e
MDJAmin <mohammadamindf@gmail.com>
2024-11-04 20:40:38 +0330

    Complete Spinner project

--- 5/js/script.js
+++ 5/js/script.js
@@ -51,3 +51,21 @@ function plus() {
   output.innerHTML = ++spinnerValue;
   meter.style.height = spinnerValue * 10 + "%";
 }
+
+function minus() {
+  if (spinnerValue <= 7) {
+    meter.style.background = "rgb(211, 211, 9)";
+    meter.style.filter =
+      "drop-shadow(0 0 2.5px rgb(229, 255, 0)) drop-shadow(0 0 10px rgb(229, 255, 0))";
+  }
+  if (spinnerValue <= 4) {
+    meter.style.background = "#0f0";
+    meter.style.filter =
+      "drop-shadow(0 0 2.5px #0f0) drop-shadow(0 0 10px #0f0)";
+  }
+  if (spinnerValue <= 0) {
+    return false;
+  }
+  output.innerHTML = --spinnerValue;
+  meter.style.height = spinnerValue * 10 + "%";
+}
