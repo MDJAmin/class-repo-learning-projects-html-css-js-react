302a609512a5a6cc8294780226bb8cabd3cc35c3
MDJAmin <mohammadamindf@gmail.com>
2025-01-04 18:44:41 +0330

    Clean code's & light \ dark mood functionality

--- Final Js Project/js/script.js
+++ Final Js Project/js/script.js
@@ -1,16 +1,10 @@
 "use strict";
-
-// handle hamburger menu functionality
 const menuIcon = document.querySelector("#menuIcon");
 const overlay = document.querySelector(".overlay");
 const navIcons = document.querySelector(".nav_icons");
 const closeButton = document.querySelector(".close_button");
-
-// Icons
-const authIcon = document.querySelector("#authIcon"); // Login/Register link
-const logoutIcon = document.querySelector("#logoutIcon"); // Logout icon
-
-// Show/Hide sidebar
+const authIcon = document.querySelector("#authIcon"); 
+const logoutIcon = document.querySelector("#logoutIcon"); 
 const showSidebar = () => {
   navIcons.classList.add("show-icons");
   overlay.classList.add("show-overlay");
@@ -31,43 +25,48 @@ menuIcon.addEventListener("click", showSidebar);
 closeButton.addEventListener("click", hideSidebar);
 overlay.addEventListener("click", hideSidebar);
 window.addEventListener("resize", handleResize);
-
-// Handle user authentication state
 const token = localStorage.getItem("token");
-
-// If user is logged in, show the logout icon and hide the login link
 if (token) {
   authIcon.style.display = "none";
   logoutIcon.style.display = "block";
 }
-
-// Handle logout
 logoutIcon.addEventListener("click", () => {
-  localStorage.removeItem("token"); // Remove token
-  window.location.href = "Pages/index.html"; // Redirect to login page
+  localStorage.removeItem("token"); 
+  window.location.href = "Pages/index.html"; 
 });
-
-// Handle animations on scroll
 const productItems = document.querySelectorAll(".product-item");
 
 const observer = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach((entry, index) => {
       if (entry.isIntersecting) {
-        // Add animation with a delay based on index
+    
         entry.target.style.animationDelay = `${index * 0.2}s`;
         entry.target.classList.add("animate");
-        observer.unobserve(entry.target); // Stop observing once animated
+        observer.unobserve(entry.target); 
       }
     });
   },
   {
-    threshold: 0.2, // Trigger when 20% of the item is visible
+    threshold: 0.2, 
   }
 );
-
-// Observe each product item
 productItems.forEach((item, index) => {
-  item.style.transitionDelay = `${index * 0.2}s`; // Add a delay to the item itself
+  item.style.transitionDelay = `${index * 0.2}s`; 
   observer.observe(item);
+});
+
+const lampIcon = document.querySelector(".Lamp_icon");
+const body = document.body;
+lampIcon.addEventListener("click", () => {
+
+  body.classList.add("theme-transition");
+
+
+  body.classList.toggle("light-mode");
+
+
+  setTimeout(() => {
+    body.classList.remove("theme-transition");
+  }, 5000);
 });
