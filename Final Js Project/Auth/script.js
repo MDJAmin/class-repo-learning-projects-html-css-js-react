4e3df06aee8118817bdf3d7de8f8f56c7705d0ea
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-03-13 20:55:13 +0330

    CC's : auth

--- Final Js Project/Auth/script.js
+++ Final Js Project/Auth/script.js
@@ -1,44 +1,3 @@
-// "use strict"
-
-// document.querySelector("#loginForm").addEventListener("submit", async function (event) {
-//     event.preventDefault()
-
-//     const username = document.querySelector("#username").value
-//     const password = document.querySelector("#password").value
-//     const massageDive = document.querySelector("#massage")
-
-//     massageDive.textContent = "";
-
-//     try {
-//         const response = await fetch("https://fakestoreapi.com/auth/login", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({
-//                 username: username,
-//                 password: password
-//             }),
-//         });
-//         if (!response.ok) {
-//             throw new Error("Invalid username or password")
-//         }
-//         const data = await response.json()
-//         const token = data.token;
-
-//         localStorage.setItem("token", token)
-
-//         massageDive.textContent = "Login successful! Redirecting..."
-
-//         setTimeout(() => {
-//             window.location.href = "../index.html"
-//         }, 2000)
-
-//     } catch (error) {
-//         massageDive.textContent = error.message
-//     }
-// })
-
 "use strict";
 
 /* =======================
@@ -68,8 +27,8 @@ const handleLoginFormSubmit = async (event) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: username,
-        password: password,
+        username: username, // mor_2314
+        password: password, // 83r5^_
       }),
     });
 
@@ -79,12 +38,8 @@ const handleLoginFormSubmit = async (event) => {
 
     const data = await response.json();
     const token = data.token;
-
-    // Store token and provide feedback to the user
     localStorage.setItem("token", token);
     messageDiv.textContent = "Login successful! Redirecting...";
-
-    // Redirect after a delay
     setTimeout(() => {
       window.location.href = "../index.html";
     }, 2000);
