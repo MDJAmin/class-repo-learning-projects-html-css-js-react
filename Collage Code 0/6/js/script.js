9bcc8ae705145ada3a3547553578c58613be23f2
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-06-12 08:13:39 +0430

    C6 JS

---
+++ Collage Code 0/6/js/script.js
@@ -0,0 +1,94 @@
+
+// // var age = 18;
+
+// let aminAge = 18 // Number  
+
+// const fname = "Amin" // String
+// const num = "12283923892" // String
+
+// let isStudent = true // Boolean
+
+// let lname; // undefined
+
+// const x = null; // null
+
+// console.log(fname,aminAge)
+// console.log(num)
+// console.log(isStudent)
+// console.log(lname)
+// console.log(x)
+
+// age=prompt("Enter You're age:")
+// console.log(age)
+
+// let thisYear = 2025;
+
+// let birthDay = 2006;
+
+// let age = thisYear - birthDay
+// console.log(age)
+
+// let num1 = 20
+// let num2 = "10"
+// let calc = num1 - num2
+// console.log(calc)
+
+// console.log(10 - "30" + "5" - 10)
+
+// let z1 = "20"+2
+// let x1 = 20+2
+// let z2 = "20"-2
+// let x2 = 20+2
+// let z3 = "20"*2
+// let x3 = 20*2
+// let z4 = "20"/2
+// let x4 = 20/2
+// console.log(z1,x1,z2,x2,z3,x3,z4,x4)
+
+// let x = 10;
+
+// x += 5 // x = x + 5
+// x -= 5 // x = x - 5
+// x *= 5 // x = x * 5
+// x++
+// x--
+
+// console.log(x)
+
+// const aminAge = 18
+// const amirAge = 18
+// console.log(aminAge >= amirAge)
+
+
+// let fName = "Amin"
+// let lName = "Jf"
+// let birthDay = 2006;
+// let job = "Developer"
+
+// // let information = fName+" "+lName+" "+ (2025-birthDay)+ " " + job
+
+// let information=`${fName} ${lName} and I'm ${2025-birthDay} years old \n\ and i am a ${job}`
+
+// console.log(information)
+
+
+
+// if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+let birthDay = +prompt("Enter you're age:")
+console.log(typeof birthDay)
+let count;
+
+if(birthDay>2000){
+count=21
+}else{
+count=20
+}
+
+console.log(count)
