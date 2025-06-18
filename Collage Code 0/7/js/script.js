dd6a99c0c52df1aae20e37e62f27878af1db6993
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-06-18 12:13:06 +0430

    C7

---
+++ Collage Code 0/7/js/script.js
@@ -0,0 +1,111 @@
+"use strict"
+
+// let myFirstNameIs = "Amin"
+// console.log(myFirstNameIs)
+
+
+// let birthYear="2006"
+// console.log(+birthYear+19)
+// console.log(Number(birthYear)+19)
+// console.log(Number(birthYear+19))
+// console.log(typeof Number(birthYear+19))
+// console.log(Number("Hello"))
+// console.log(String(18))
+// console.log(typeof String(18))
+
+// console.log("19"-9+"5")
+
+// console.log(Boolean(1))
+// console.log(Boolean(0))
+// console.log(Boolean(""))
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+
+// let money;
+
+// if(money){
+//     alert("Yeyyy!")
+// }else{
+//     alert("we are poor!")
+// }
+
+// let num1 = 10
+// let num2 = "10"
+
+// if(num1===num2){
+//     console.log("equal")
+// }else{
+//     console.log("not equal")
+// }
+
+
+// const driverLicense = true
+// const goodVision = false
+// console.log(driverLicense && goodVision)
+// console.log(driverLicense || goodVision)
+
+// if (driverLicense && goodVision) {
+//     console.log("You can drive a car")
+
+// } else {
+//     console.log("You can't drive a car")
+// }
+
+// let number = prompt("Enter your Number", 10)
+
+// switch (number) {
+//     case "10":
+//         console.log("Your number is 10")
+//     break
+//         case "9":
+//         console.log("Your number is 9")
+//     break
+//         case "8":
+//         console.log("Your number is 8")
+//     break
+//         case "7":
+//         console.log("Your number is 7")
+//     break
+//         case "6":
+//         console.log("Your number is 6")
+//     break
+//     default:
+//         console.log("Unvalid Number")
+// }
+
+// let nextClass=false
+// let passExam= false
+
+
+// if(!passExam){
+//     nextClass=true
+// }
+
+// console.log(nextClass)
+
+// function logout(){
+//     console.log("hello")
+// }
+
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+// logout()
+
+
+function sum(x,y){
+ let totalNum=x+y
+return totalNum
+}
+
+console.log(sum(5,8))
