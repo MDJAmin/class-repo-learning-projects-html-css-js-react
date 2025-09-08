b7283c57a6611e3d12c3c7b94358c7ab6347c4ef
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-09-08 19:22:34 +0430

    Code 1 - C7

---
+++ Collage Code 1/7/js/index2.js
@@ -0,0 +1,25 @@
+// var fName = 'Amin'
+// // console.log(fName)
+
+
+// console.log(15 + 15)
+// console.log("15" + 15)
+// console.log("15" + "15")
+// console.log("Hi " + "How are you")
+// console.log("30" - "10")
+// console.log("hi" - 10) // NaN = Not a Number
+// console.log("hi" - "HELLO") 
+// console.log("hi" * "HELLO") 
+// console.log("hi" / "HELLO") 
+// console.log(5 /  5) 
+// console.log(5 *  5) 
+// console.log(5 ** 5) 
+
+
+const name1 = prompt("Enter your name","John")
+const name2 = prompt("Enter your name")
+const name3 = prompt()
+
+console.log(name1)
+console.log(name2)
+console.log(name3)
---
+++ Collage Code 1/7/js/script.js
@@ -0,0 +1,77 @@
+// var fNameAminLight = 'Amin'
+// var FirstNameAminLight = 'Amin'
+// var fName = 'Amin'
+// var fName = "Morteza"; // string
+// console.log(fName);
+
+var stringAge = "19"; // string
+// console.log(stringAge);
+
+console.log("Lorem ipsum's dolor sit amet")
+const text2 = "Lorem ipsum\'s dolor sit amet"
+console.log(text2)
+
+// const text = 'Lorem ipsum's dolor sit amet.'
+// const text = 'Lorem ipsum\'s dolor sit amet.'
+const text = "Lorem ipsum's dolor sit amet.";
+
+// var numberAge = 19 // Number
+// console.log(numberAge)
+
+var numberAge = 19 + 5;
+// console.log(numberAge);
+
+// 1- var
+// var a = ""
+// var b = 11349
+
+// 2- const
+// const a = ""
+// const a = ""
+
+// 3- let
+let a = "";
+
+null;
+undefined;
+
+// alert("Hello")
+
+const person = {
+  name: "amin",
+  age: 19,
+};
+
+const car = {
+  brand: "bmw",
+  model: 2016,
+};
+
+// console.log(person.age)
+
+// alert(person.name)
+
+// console.log(true)
+// console.log(false)
+
+// console.log("Hello" == 14)
+// console.log(14 == 14)
+
+let fName = "Amin";
+let age = 19;
+
+function Morteza() {
+  a + b;
+}
+
+console.log(typeof 5); // number
+console.log(typeof fName); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof condition); // function
+
+const numString = "15" + "30"
+console.log(numString)
