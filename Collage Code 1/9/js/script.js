6fca501f39c66bb4251f3f4eb16888f3c3c6b61c
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-09-26 21:17:58 +0330

    Code 1 - C9

---
+++ Collage Code 1/9/js/script.js
@@ -0,0 +1,117 @@
+// var
+// const
+// let
+
+// console.log(first)
+// alert
+// prompt
+
+// console.log(5+5)
+// console.log(10-5)
+// console.log(5*5)
+// console.log(5**5)
+
+// alert("hello")
+
+// let a
+// let a = prompt("enter name")
+// alert(a)
+// console.log(a)
+
+// console.log("Hello" + " World")
+// console.log("Hello" - " World") // Not a Number
+// console.log("Hello" * " World") // Not a Number
+
+// console.log(3 > 6) // false
+// console.log(3 < 6) // true
+// console.log(3 >= 6)
+// console.log(3 <= 6)
+// console.log(3 == 6)
+// console.log(3 === 6)
+// console.log(6 == 6)
+// console.log(6 == "6")
+// console.log(6 === 6)
+// console.log(6 === "6")
+// console.log("Hello" === "Hello") // true
+
+let a = 10;
+// console.log(a + 8)
+// console.log(a++) // +1
+// console.log(a--) // -1
+
+// console.log(a)
+// console.log(a--)
+// console.log(a++)
+
+// let number = 5;
+// if (number % 2 === 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// if (4 + 3 === 7) {
+//     alert("true")
+// }
+
+// let age = +prompt("Enter your age");
+// if (age > 13) {
+//   alert("You can enter");
+// } else {
+//   alert("You can't enter");
+// }
+// console.log(typeof age);
+
+// let fName = prompt("Enter your name")
+// console.log(typeof fName)
+
+// let name1 = "Amin"
+// let name2 = "Marzieh"
+
+// if (name === "Amin"){
+//     console.log("Hello Amin")
+// }
+
+// const num1 = 30
+// const num2 = 30
+
+// if(num1 == 20){
+// console.log(20)
+// } else if (num1 == 30){
+// console.log(30)
+// }else{
+// console.log("number not found!")
+// }
+
+// || &&
+// const name = prompt()
+// if(name === "Amin" || name ==="parsa"){
+//     console.log("welcome" + name)
+// }else{
+//     console.log()
+// }
+
+// if(name === "Amin" && name ==="parsa"){
+//     console.log("welcome" + name)
+// }else{
+//     console.log()
+// }
+
+// let age = prompt("Enter your age");
+// if (age >= 18 && age <= 70) {
+//   alert("Welcome");
+// } else {
+//   alert("you cant enter!");
+// }
+
+// condition ? x : y// ternary operator
+
+//  function isV(bool){
+//     return bool
+//  }
+
+// let answer = isV(false) ? "you can enter" : "you cant enter"
+
+let myNumber = +prompt("Enter you'r number")
+let numberChecker = myNumber > 10 ? "Grater than 10" : "Lower than 10"
+alert(numberChecker)
