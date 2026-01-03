0cd3de9585d5cfee6de33e524c026bc445ba26aa
MDJAmin <mohammadamindf@gmail.com>
2026-01-03 19:55:23 +0330

    Code 2 - Class 6

---
+++ Block Chain Collage Code 2/6/js/script.js
@@ -0,0 +1,143 @@
+"use strict";
+
+// console.log("Hello!");
+
+// Javascript types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. Object
+
+// ---------------------------
+
+// 1. Number
+// console.log(19);
+// console.log("19");
+// console.log(Number("131"));
+// console.log(Number("Hello")); // NaN = Not a Number
+
+// ---------------------------
+
+// 2. String
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("There's No one Here");
+// console.log("There's No one Here");
+
+// ---------------------------
+
+// 3. Boolean
+true; // 1
+false; // 0
+
+// console.log(true);
+// console.log(false);
+
+// ---------------------------
+
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+
+// var name
+// let name
+// const name
+
+// var fName = "Amin";
+var age = 19;
+var description =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+// console.log(description);
+// console.log("Hello My name is " + fName + " And i'm " + age + " I do " + description)
+// console.log(`Hello My name is ${fName}`)
+// - , + , / , * , %
+// **
+
+// let & const
+
+// var text = "Hello World!"
+// var text = "dvheqwoihvek"
+
+// console.log(text)
+
+const idNumber = "0912345678";
+
+// let fName = "Amin";
+
+// idNumber = "1343214"
+
+// fName = "Amir";
+
+// console.log(fName)
+
+// let amin&ali
+// let 2141amin = "ascwa"
+let amin131 = "123";
+
+// let _name
+
+// let $name
+
+// "hello-world"
+// "hello_world"
+
+// let my_text;
+
+// let MyText;
+
+// let myTextIsHere;
+
+// console.log(my_text)
+// console.log(myTextIsHere)
+
+// var text = "Hello"
+// console.log(text)
+// var text = "Hello2"
+// console.log(text)
+// var text;
+// console.log(text)
+
+// var text2;
+// console.log(text2)
+
+let b = 7;
+let a = 1;
+// console.log(a);
+a = b;
+// console.log("a:", a);
+// console.log("b:", b);
+
+a = "Hello world!";
+// console.log(a);
+
+const c = 15;
+// c = 10 ❌
+
+const number2 = 1;
+const string = "Batman";
+const boolean = true;
+
+// console.log(typeof number2)
+// console.log(typeof string)
+// console.log(typeof boolean)
+// console.log(typeof NaN)
+
+// let fName = prompt("Enter You'r name","Amin")
+// console.log(fName);
+
+let firstNumber = +prompt("Enter first Number");
+let secondNumber = +prompt("Enter second Number");
+let lastNumber = +prompt("Enter last Number");
+
+const sum = firstNumber + secondNumber + lastNumber;
+
+console.log(sum);
