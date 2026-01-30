af84bbb2ca60159b7c3480a5d1cc923c69bb984f
MDJAmin <mohammadamindf@gmail.com>
2026-01-30 14:16:33 +0330

    Code 2 - Class 11

---
+++ Block Chain Collage Code 2/11/js/script.js
@@ -0,0 +1,237 @@
+// for (let i = 1; i <= 6; i++) {
+//   console.log("Number: " + i);
+// }
+
+// let todos = [
+//  "Task 1", // = i
+//  "Task 2", // = i
+//  "Task 3", // = i
+//  "Task 4",
+//  "Task 5",
+//  "Task 6",
+//  "Task 7",
+//  "Task 8",
+//  "Task 9",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+//  "Task 10",
+// ];
+
+// console.log(todos.length);
+
+// const todosLength = todos.length;
+
+// for (let i = 0; i < todosLength; i++) {
+//   todos[i] = "▢ " + todos[i];
+//   // todos.pop()
+// }
+
+// console.log(todos);
+
+// let counterOne = 10
+// while (counterOne>0){
+//     console.log(counterOne)
+//     counterOne++
+// }
+
+// let counterTwo = 10;
+// do {
+//   console.log(counterTwo);
+//   counterTwo--;
+// } while (counterTwo >= 0);
+
+let todos = [
+  "Task 1",
+  "Task 2",
+  "Task 3",
+  "Task 4",
+  "Task 5",
+  "Task 6",
+  "Task 7",
+  "Task 8",
+  "Task 9",
+  "Task 10",
+];
+
+// let todoLength = todos.length;
+// for (let i = 0; i < todoLength; i++) {
+//   console.log(todos[i], i);
+// }
+
+// array.forEach(element => {
+
+// });
+
+// todos.forEach(function (todos, i) {
+//   console.log(todos, i);
+// });
+
+
+// function logTodos(todos,i){
+//     console.log(todos,i)
+// }
+
+// todos.forEach(logTodos)
+
+// let animals = ["cat","dog","snake","wolf","lion","crow"]
+// animals.push("bird")
+// animals.forEach(function(an){
+//     console.log(an)
+// })
+
+
