f9449ec61f746ad51d3a3d772b08ff131edcbcc4
MDJAmin <mohammadamindf@gmail.com>
2026-02-26 15:15:23 +0330

    Code 2 - Class 14 / 1

---
+++ Block Chain Collage Code 2/14/1/js/script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const openBtn = document.querySelector("#open-modal")
+const closeBtn = document.querySelector("#close-modal")
+const modalEl = document.querySelector(".modal")
+
+
+openBtn.addEventListener("click",()=>{
+    modalEl.classList.add("show")
+})
+
+closeBtn.addEventListener("click",()=>{
+    modalEl.classList.remove("show")
+})
+
+setTimeout(()=>{
+    modalEl.classList.add("show")
+}, 5000)
