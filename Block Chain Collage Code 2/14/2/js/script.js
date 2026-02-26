bea609d308b8cac02f267dc90192947263d70796
MDJAmin <mohammadamindf@gmail.com>
2026-02-26 15:15:35 +0330

    Code 2 - Class 14 / 2

---
+++ Block Chain Collage Code 2/14/2/js/script.js
@@ -0,0 +1,47 @@
+// let x  = Math.random()
+// console.log(x)
+
+// let y = Math.trunc(4.97384729)
+// console.log(y)
+
+// let z = Math.floor(9.98)
+// console.log(z)
+
+// let a = Math.ceil(8.2)
+// console.log(a)
+
+// let b = Math.round(8.7)
+// console.log(b)
+
+// let c = Math.max(4, 6, 9, 14, 57, 32);
+// console.log(c);
+
+// let e = Math.min(4, 6, 9, 14, 57, 32);
+// console.log(e);
+
+// let f = Math.pow(4,6)
+// console.log(f)
+
+// let q = Math.sqrt(16)
+// console.log(q)
+
+// let randomNumber = Math.floor(Math.random() * 200 + 1);
+// console.log(randomNumber);
+
+// setTimeout(() => {
+//   console.log(0);
+// }, 0); // 3
+
+// console.log(1); // 1
+
+// setTimeout(() => {
+//   console.log(2);
+// }, 1000); // 5
+
+// setTimeout(() => {
+//   console.log(3);
+// }, 500); // 4
+
+// console.log(4); // 2
+
+
